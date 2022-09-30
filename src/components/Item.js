e772fd35daa6f0e76481cc246accf9e00b76ed5a
Steven McVey <smcvey1210@gmail.com>
2022-09-29 22:39:21 -0700

    commit tree 44ee706d996905198f24e27e9308e29440ad51ba

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,40 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdate, onItemDeleted }) {
+
+  function handleAddToCartClick(){
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+    .then(res=>res.json())
+    .then(updatedItem=>onUpdate(updatedItem))
+  }
+
+  function handleDelete(){
+    fetch(`http://localhost:4000/items/${item.id}`,{
+      method: "DELETE",
+      headers: {
+        "Content-Type": "Application/json",
+      }
+    })
+    .then(r=>r.json())
+    .then(()=>onItemDeleted(item.id))
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button onClick={handleAddToCartClick} className={item.isInCart ? "remove" : "add"}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button onClick={handleDelete} className="remove">Delete</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,8 +7,34 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  useEffect(()=>{
+    fetch("http://localhost:4000/items")
+    .then(res=>res.json())
+    .then(data=>setItems(data))
+  }, [])
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
+  }
+
+  function handleAddItem(newItem){
+    setItems([...items, newItem])
+  }
+
+  function handleUpdate(updatedValue){
+    const updatedItems = items.map((item)=>{
+      if (item.id === updatedValue.id){
+        return updatedValue;
+      } else return item;
+    })
+    setItems(updatedItems)
+  }
+
+  function handleDeletedItem(itemID){
+    const updatedList = items.filter((item)=>{
+      return item.id !== itemID
+    })
+    setItems(updatedList)
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -19,14 +45,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item onItemDeleted={handleDeletedItem} onUpdate={handleUpdate} key={item.id} item={item} />
         ))}
       </ul>
     </div>
